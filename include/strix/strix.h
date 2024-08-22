477e5f6cce6ec8314fa271fcc67c9c84502e8f74
biog4m3r <biog4m3ryt@gmail.com>
2024-08-22 20:33:25 -0300

    Script system

--- include/strix/strix.h
+++ include/strix/strix.h
@@ -29,4 +29,5 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+
 #endif // STRIX_H
--- src/strix_test.cpp
+++ src/strix_test.cpp
@@ -1,58 +1,32 @@
 #include <strix/strix.h>
-#include <strix/strix_logger.h>
 #include <strix/strix_platform.h>
 #include <strix/strix_gl.h>
-#include <strix/strix_game.h>
+#include <strix/strix_logger.h>
 
-#include <iostream>
-#include <string>
+using namespace strix;
 
 int strixMain()
 {
-    if (!strix::Platform::initOpenGL(3, 3))
+    if (!Platform::initOpenGL(3, 3))
     {
-        strix::Logger::Error("Failed to initialize OpenGL");
+        Logger::Error("Failed to initialize OpenGL");
         return -1;
     }
-
-    strix::Window *window = strix::Platform::createWindow(800, 600, "Strix Engine");
-
+    Window *window = Platform::createWindow(800, 600, "Strix Engine");
     if (!window)
     {
-        strix::Logger::Error("Failed to create window");
+        Logger::Error("Failed to create window");
         return -1;
     }
 
-    strix::Platform::setShouldClose(window, false);
-
-    strix::Module *game = strix::Platform::loadModule("libgame.dll");
-    strix::STRIX_GAME_CALLBACK_ONSTART onGameStart = (strix::STRIX_GAME_CALLBACK_ONSTART)
-        strix::Platform::getModuleFunction(game, strix::STRIX_CALLBACK_NAME_ONSTART);
-
-    strix::STRIX_GAME_CALLBACK_ONUPDATE onGameUpdate = (strix::STRIX_GAME_CALLBACK_ONUPDATE)
-        strix::Platform::getModuleFunction(game, strix::STRIX_CALLBACK_NAME_ONUPDATE);
-
-    strix::STRIX_GAME_CALLBACK_ONRENDER onGameRender = (strix::STRIX_GAME_CALLBACK_ONRENDER)
-        strix::Platform::getModuleFunction(game, strix::STRIX_CALLBACK_NAME_ONRENDER);
-
-    strix::STRIX_GAME_CALLBACK_ONSTOP onGameStop = (strix::STRIX_GAME_CALLBACK_ONSTOP)
-        strix::Platform::getModuleFunction(game, strix::STRIX_CALLBACK_NAME_ONSTOP);
-
-    onGameStart();
-
-    while (!strix::Platform::getShouldClose(window))
+    while (!Platform::getShouldClose(window))
     {
-        strix::Platform::pollEvents(window);
+        Platform::pollEvents(window);
 
-        onGameUpdate(0.0f);
-        onGameRender();
-
-        strix::Platform::swapBuffers(window);
+        Platform::swapBuffers(window);
     }
 
-    onGameStop();
-
-    strix::Platform::destroyWindow(window);
+    Platform::destroyWindow(window);
 
     return 0;
 }
